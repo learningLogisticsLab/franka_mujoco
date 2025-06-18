470ed72226b6eaafefb470d64d1d137da37f4cd3
ryanvanderstelt <ryan@vstelt.com>
2025-06-18 17:31:04 -0500

    train.py working, updated requirements.txt

---
+++ examples/push/tqc/train.py
@@ -0,0 +1,32 @@
+import gymnasium as gym
+from panda_mujoco_gym.envs.push import FrankaPushEnv
+from stable_baselines3 import HerReplayBuffer
+from sb3_contrib import TQC
+
+env = gym.make("FrankaPushSparse-v0", render_mode="human")
+
+policy_kwargs = dict(n_critics=2, n_quantiles=25)
+
+model = TQC(
+    "MultiInputPolicy", 
+    env, 
+    replay_buffer_class=HerReplayBuffer,
+    top_quantiles_to_drop_per_net=2, 
+    verbose=1, 
+    tensorboard_log="./examples/push/tqc/logs",
+    policy_kwargs=policy_kwargs
+)
+model.learn(total_timesteps=10_000, log_interval=4)
+model.save("tqc_push")
+
+# del model
+
+# model = TQC.load("tqc_push")
+
+# obs, _ = env.reset()
+# while True:
+#     action, _states = model.predict(obs, deterministic=True)
+#     obs, reward, terminated, truncated, info = env.step(action)
+#     env.render()
+#     if terminated or truncated:
+#         obs, _ = env.reset()
